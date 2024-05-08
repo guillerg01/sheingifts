c10bf216b42baf155ec40c984880db833ead3cbf
CynthiaBarba03 <141068051+CynthiaBarba03@users.noreply.github.com>
2024-05-08 18:06:38 -0500

    act page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main  className="flex justify-center items-center w-screen h-screen">
-    <h1 className="text-gray-900 text-xl">Home</h1>
+    <h1 className="text-gray-900 text-xl">Homee</h1>
     </main>
   );
 }
