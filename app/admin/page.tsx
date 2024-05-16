f4c1d668fa52feed9b9b2310672f036a05b4c970
Guillermo Enrique Rodríguez Galban <93445026+guillerg01@users.noreply.github.com>
2024-05-16 18:38:24 -0400

    update admin

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import { Sidebar } from './Componentes/sidebar'
+
+export default function page(){
+  return (
+
+    <div>
+        <Sidebar></Sidebar>
+    </div>
+  )
+}
