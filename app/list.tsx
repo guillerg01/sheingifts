b9820c25f29a81ec151a94c66005bb75ceeafbbf
Calana2 <Protoncala@proton.me>
2024-05-16 22:19:35 -0400

    0.0

--- app/list.tsx
+++ app/list.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import axiosInstancePrivate from "@/service/axiosInstancePrivate";
+import { useState } from "react";
+
 export default function List(){
+ const [list, setList] = useState('');
+
+const getList = async () => {
+ try {
+   const users = await axiosInstancePrivate.get('/api/list/all'); 
+   setList(users);
+ } catch (err) {
+   console.log(err);
+ }
+}
+
+getList();
+
  return(
   <div>
-   List component
+   List component 
+    {list.length}
   </div>
  );
 }
