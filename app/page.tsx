9fc13e3aea93fa423db129bcfb9ac88f51317b78
Calana2 <Protoncala@proton.me>
2024-05-25 22:48:21 -0400

    minimoFinalizado

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,7 @@ export default function Home() {
 
   return (
     <main  className="flex justify-center items-center w-screen h-screen">
-   {/* <h1 className="text-gray-900 text-xl">Homee</h1> */}
-
-    <div className="p-5 flex items-center justify-center"><List/></div>
+    <h1 className="text-gray-900 text-xl">Homee</h1>
     </main>
   );
 }
